77640ea27aea95d48b777e49bf4c2aa6c9e694b7
Mohit Khairia <113705293+mohitkhairia@users.noreply.github.com>
2023-03-04 14:58:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,50 +1,70 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import axios from "axios";
+import "./App.css";
+import Home from "./pages/Home.js";
+import Signup from "./pages/Signup.js";
+import Login from "./pages/Login";
+import Posts from "./pages/Posts";
+import Navbar from "./components/Navbar.js";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userPosts, setUserPosts] = useState([]);
+  const [postToUpdate, setPostToUpdate] = useState(null);
 
-  handleClick = api => e => {
-    e.preventDefault()
+  const handleSignup = (credentials) => {
+    // Handle signup logic
+    setIsLoggedIn(true);
+  };
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+  const handleLogin = (credentials) => {
+    // Handle login logic
+    setIsLoggedIn(true);
+    setUserPosts([
+      { id: 1, title: 'My first post', content: 'Lorem ipsum dolor sit amet.' },
+      { id: 2, title: 'My second post', content: 'Consectetur adipiscing elit.' },
+      { id: 3, title: 'My third post', content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    ]);
+  };
 
-  render() {
-    const { loading, msg } = this.state
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUserPosts([]);
+    setPostToUpdate(null);
+  };
+  const handleDelete = (postId) => {
+    // Filter out the post with the given postId from userPosts
+    const updatedPosts = userPosts.filter((post) => post.id !== postId);
+    setUserPosts(updatedPosts);
+  };
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+  const handleUpdate = (postId, { title, content }) => {
+    // Find the post with the given postId in userPosts and update it
+    const updatedPosts = userPosts.map((post) => {
+      if (post.id === postId) {
+        return { id: post.id, title, content };
+      } else {
+        return post;
+      }
+    });
+    setUserPosts(updatedPosts);
+  };
+  
+  return (
+    <>
+      <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      {isLoggedIn ? (
+        <Posts userPosts={userPosts} onDelete={handleDelete} onUpdate={handleUpdate} onLogout={handleLogout} postToUpdate={postToUpdate} setPostToUpdate={setPostToUpdate} />
+      ) : (
+        <>
+          <Home />
+          <Signup onSignup={handleSignup} />
+          <Login onLogin={handleLogin} />
+        </>
+      )}
+    </>
+  );
+};
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
